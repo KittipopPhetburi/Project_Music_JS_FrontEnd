bc0c55655e3eebc07f4138be04f62e7e7fdf9505
Apiwat89 <apiwat89123@gmail.com>
2024-02-29 00:07:55 +0700

    updata day 2

--- AxiosNodeHTML.js
+++ AxiosNodeHTML.js
@@ -2,8 +2,9 @@ const path = require("path");
 const express = require('express');
 const axios = require('axios');
 const app = express();
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
@@ -18,15 +19,174 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Serve static files
 app.use(express.static (__dirname + '/public'));
 
-app.use(cookieParser());
+app.use(cookieParser()); // เรียกใช้ cookie
+
+const music = multer.diskStorage({
+    destination: function (req,file,cb) {
+        cb(null, path.join(__dirname, './public/Pictures'));
+    },
+    filename: function (req,file,cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const ms = multer({storage: music});
 
 app.get("/", async (req, res) => {
     try {
         const response = await axios.get(base_url + '/music'); //เข้าถึงDB music
-        res.render("home", { musics: response.data, Role: req.cookies.role, User: req.cookies.username});
+        return res.render("home", { musics: response.data, Role: req.cookies.role, User: req.cookies.username});
     } catch (err) {
         console.error(err);
-        res.status(500).send('Error root');
+        res.status(500).send('Error /');
+    }
+});
+
+app.get("/info", async (req, res) => {
+    try {
+        const response = await axios.get(base_url + '/user');
+        const users = response.data;
+
+        for (let user of users) {
+            if (req.cookies.username === user.username) {
+                const response2 = await axios.get(base_url + '/user/' + user.user_id);
+                return res.render("info", { users: response2.data, Role: req.cookies.role, User: req.cookies.username});
+            }
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error info');
+    }
+});
+
+app.get("/admin", async (req, res) => {
+    try {
+        if (req.cookies.role === 'admin') {
+            const response = await axios.get(base_url + "/music");
+            return res.render("admin", { musics: response.data, Role: req.cookies.role, User: req.cookies.username});
+        } else if (req.cookies.role === 'user') {
+            return res.redirect("/");
+        } else {
+            return res.redirect("/");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error admin');
+    }
+});
+
+app.get("/music_detail_admin/:id", async (req, res) => {
+    try {
+        if (req.cookies.role === 'admin') {
+            const response = await axios.get(base_url + '/music/' + req.params.id); //เข้าถึงDB music และเข้าถึง id
+            return res.render("music_detail_admin", { muscis_detail: response.data, Role: req.cookies.role, User: req.cookies.username }); // ส่งค่าไปหน้า playlist_hit ใน {} ซ้าย เป็นตัวแปรและเก็บค่า data
+        } else if (req.cookies.role === 'user') {
+            return res.redirect("/");
+        } else {
+            return res.redirect("/");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error music_detail_admin');
+    }
+});
+
+app.get("/music_add_admin", async (req, res) => {
+    try {
+        if (req.cookies.role === 'admin') {
+            return res.render("music_add_admin", { Role: req.cookies.role, User: req.cookies.username }); // ส่งค่าไปหน้า playlist_hit ใน {} ซ้าย เป็นตัวแปรและเก็บค่า data
+        } else if (req.cookies.role === 'user') {
+            return res.redirect("/");
+        } else {
+            return res.redirect("/");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error music_add_admin');
+    }
+});
+
+app.post("/createMusic", ms.single('musicImg') ,async (req, res) => {
+    try {
+        if (req.cookies.role === 'admin') {
+            const data = {
+                title: req.body.title,
+                singer: req.body.singer,
+                genre: req.body.genre,
+                release_date: req.body.release_date,
+                musicImg: req.file.filename
+            }
+            
+            await axios.post(base_url + "/music" , data);
+            return res.redirect("/admin");
+        } else if (req.cookies.role === 'user') {
+            return res.redirect("/");
+        } else {
+            return res.redirect("/");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error createMusic');
+    }
+});
+
+app.get("/music_edit_admin/:id", async (req, res) => {
+    try {
+        if (req.cookies.role === 'admin') {
+            const response = await axios.get(base_url + "/music/" + req.params.id);
+            return res.render("music_edit_admin", { music: response.data, Role: req.cookies.role, User: req.cookies.username }); // ส่งค่าไปหน้า playlist_hit ใน {} ซ้าย เป็นตัวแปรและเก็บค่า data
+        } else if (req.cookies.role === 'user') {
+            return res.redirect("/");
+        } else {
+            return res.redirect("/");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error music_edit_admin');
+    }
+});
+
+app.post("/editMusic/:id", ms.single('musicImg') ,async (req, res) => {
+    try {
+        if (req.cookies.role === 'admin') {
+            const response = await axios.get(base_url + "/music/" + req.params.id);
+            const music = response.data;
+
+            const data = {
+                title: req.body.title === '' ? music.title : req.body.title,
+                singer: req.body.singer === '' ? music.singer : req.body.singer,
+                genre: req.body.genre === '' ? music.genre : req.body.genre,
+                release_date: req.body.release_date === '' ? music.release_date : req.body.release_date,
+                musicImg: !req.file ? music.musicImg : req.file.filename
+            }
+            
+            await axios.put(base_url + "/music/" + req.params.id, data);
+            return res.redirect("/admin");
+        } else if (req.cookies.role === 'user') {
+            return res.redirect("/");
+        } else {
+            return res.redirect("/");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error editMusic');
+    }
+});
+
+app.get("/music_delete_admin/:id", async (req, res) => {
+    try {
+        if (req.cookies.role === 'admin') {
+            await axios.delete(base_url + "/music/" + req.params.id)
+            const response = await axios.get(base_url + "/music");
+            return res.render("admin", {  musics: response.data, Role: req.cookies.role, User: req.cookies.username }); // ส่งค่าไปหน้า playlist_hit ใน {} ซ้าย เป็นตัวแปรและเก็บค่า data
+        } else if (req.cookies.role === 'user') {
+            return res.redirect("/");
+        } else {
+            return res.redirect("/");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error music_detail');
     }
 });
 
@@ -60,7 +220,7 @@ app.get("/playlist_love", async (req, res) => {
         let datas = []; // สร้างตัวแปรมาเก็บข้อมูล
         for (let music of musics) {
             if (music.genre === 'รัก') { // check genre
-                const response2 = await axios.get(base_url + '/music/' + music.id); // หากเจอ genre เศร้าสามารถดึงข้อมูลมาทั้งแถว
+                const response2 = await axios.get(base_url + '/music/' + music.id); // หากเจอ genre เศร้าสามารถดึงข้อมูลมาทั้งแถวด้วย music.id
                 const data = response2.data; // เอาข้อมูลของแถวมาเก็บในตัวแปร
                 datas.push(data); // ใส่ข้อมูลลงไปใน array
             }
@@ -190,6 +350,80 @@ app.post("/register2", async (req,res) => {
     }
 })
 
+app.get("/resetpass", (req,res) => {
+    try {
+        return res.render("resetpass", {Fail:""});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error resetpass');
+    }
+})
+
+app.post("/resetpass", async (req,res) => {
+    try {
+        const response = await axios.get(base_url + "/user");
+        const users = response.data;
+
+        let checkFail = true;
+        for (let user of users){
+            if(req.body.username === user.username){
+                if(req.body.email === user.email){
+                    checkFail = false;
+                    res.cookie('resetpass', user.username, {maxAge: 9000000, httpOnly: true});
+                    return res.render("resetpass2", {Fail: ""});
+                }
+            }
+        }
+
+        if (checkFail) {
+            return res.render("resetpass", {Fail: "ชื่อผู้ใช้หรืออีเมล์ไม่ถูกต้อง"}); // ส่งข้อมูลของ role และ fail เช็คว่าชื่อผู้ใช้กับรหัสผิดหรือไม่ ไปหน้า login 
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error resetpass');
+    }
+})
+
+app.get("/resetpass2", (req,res) => {
+    try {
+        return res.render("resetpass2", {Fail:""});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error resetpass2');
+    }
+})
+
+app.post("/resetpass2", async (req,res) => {
+    try {
+        let checkFail = true;
+        if(req.body.new_password === req.body.con_password){
+            checkFail = false;
+        }
+
+        if (checkFail) { // เช็คว่ารหัสผ่านตรงกันหรือไม่ 
+            return res.render("resetpass2", {Fail: "รหัสผ่านไม่ตรงกัน"}); // ส่งข้อมูลของ role และ fail เช็คว่าชื่อผู้ใช้กับรหัสผิดหรือไม่ ไปหน้า login 
+        } else {
+            const response = await axios.get(base_url + "/user");
+            const users = response.data;
+    
+            for (let user of users){
+                if(req.cookies.resetpass === user.username){
+                    const data = {
+                        password: req.body.new_password
+                    }
+                    await axios.put(base_url + "/user/" +  user.user_id, data);
+                    res.clearCookie('resetpass');
+                }
+            }
+
+            return res.redirect("/");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error resetpass2');
+    }
+})
+
 app.get("/logout", (req,res) => {
     try {
         res.clearCookie('role');
@@ -197,7 +431,7 @@ app.get("/logout", (req,res) => {
         return res.redirect("/");
     } catch (err) {
         console.error(err);
-        res.status(500).send('Error register');
+        res.status(500).send('Error logout');
     }
 })
 
